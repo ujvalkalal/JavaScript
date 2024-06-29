8c8ed180c687181ca675503053d64bff5caa0578
ujval <naveenkuamr.gs@isteer.com>
2024-06-29 22:58:29 +0530

    Datattypes and variable

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodeJs not Browser engine so this is not allowed
+
+console.log(3 + 3);
+``;
+console.log("ujval");
+
+//primitive data types
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => value not defined yet
+// symbol => unique value
+
+//non-primitive data-types
+// object => key value pair
+console.log(typeof undefined); //this will output the undefined
+console.log(typeof null); //this will output the Object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 11654;
+let accountEmail = "ujval@gmail.com";
+var state = "karnataka";
+let accountName = "ujval";
+city = "bengaluru";
+
+//accountId = 765436;
+
+/*
+never use the var as you might face the scope issues in the future
+*/
+console.log(accountId);
