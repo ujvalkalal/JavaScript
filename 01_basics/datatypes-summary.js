e4a2a02cafca6bbf8919467e37def9c4cd2f7ec9
ujval <naveenkuamr.gs@isteer.com>
2024-06-30 20:42:59 +0530

    Heap and stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -38,3 +38,26 @@ myFunction();
 
 //link to read the return types
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++Memory Lessions++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myRole = "developer";
+let anotherRole = myRole;
+anotherRole = "Cloud Engineer";
+
+console.log(myRole);
+console.log(anotherRole);
+
+let user1 = {
+  role: "developer",
+  emailId: "ujval@google.com",
+};
+
+let user2 = user1;
+
+user2.role = "Cloud Engineer";
+
+console.log(user1);
+console.log(user2);
