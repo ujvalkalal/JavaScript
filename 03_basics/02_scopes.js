aa511ef6b13d34549aa8ac8caaccb8d63391d35f
ujval <naveenkuamr.gs@isteer.com>
2024-07-10 15:53:13 +0530

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -17,10 +17,43 @@
 // console.log(b);
 // console.log(c);
 
-let a = 400;
-if (true) {
-  let a = 40;
-  const b = 50;
-  console.log("Inner Scope Value is", a);
+// let a = 400;
+// if (true) {
+//   let a = 40;
+//   const b = 50;
+//   console.log("Inner Scope Value is", a);
+// }
+// console.log(a);
+
+//Function Scopes
+
+function one() {
+  const username = "ujval";
+  function two() {
+    const website = "www.ujval.com";
+    console.log(website);
+  }
+  //   console.log(website);
+  two();
 }
-console.log(a);
+//one();
+
+if (true) {
+  const username = "ujval";
+  if (username === "ujval") {
+    const website = " youtube";
+    //console.log(username + website);
+  }
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++Intresting++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+// console.log(addTwo(5));
+const addTwo = function (num2) {
+  return num2 + 2;
+};
