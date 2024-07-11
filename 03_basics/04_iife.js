1c58d4ef8e7e2bc2dbfb8b699bd985d4c4ff9d62
ujval <naveenkuamr.gs@isteer.com>
2024-07-11 14:25:03 +0530

    iife details

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,18 @@
+//(IIFE) Immediately Invoked function Expressions
+
+//below is the named iife
+(function check1() {
+  console.log("db Connected 1");
+})();
+(function check2() {
+  console.log("db Connected 2");
+})();
+
+//unnamed iife
+(() => {
+  console.log("db Connected 3");
+})();
+
+((name) => {
+  console.log(`db Connected to ${name}`);
+})("Ujval");
