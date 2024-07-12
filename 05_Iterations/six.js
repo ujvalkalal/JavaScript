6e5f5c25211afec7c5be27a3551aae76e0002df9
ujval <naveenkuamr.gs@isteer.com>
2024-07-12 17:27:49 +0530

    Array of objects iteraring using the filter

---
+++ 05_Iterations/six.js
@@ -0,0 +1,58 @@
+//const coding = ["js", "php", "ruby", "java", "html", "css"];
+// const values = coding.forEach((item) => {
+//   console.log(item);
+//   return item;
+// });
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums = myNums.filter((num) => num > 4);
+// console.log(newNums);
+
+// myNums.forEach((item) => {
+//   if (item > 4) console.log(item);
+// });
+
+// const newNums1 = [];
+// myNums.forEach((item) => {
+//   if (item > 4) newNums1.push(item);
+// });
+// console.log(newNums1);
+
+//generate the array of objects for the books include details such as title, genre, publish, edition
+const books = [
+  {
+    title: "book1",
+    genre: "fiction",
+    publish: 2020,
+    edition: 1,
+  },
+  {
+    title: "book2",
+    genre: "non-fiction",
+    publish: 2021,
+    edition: 2,
+  },
+  {
+    title: "book3",
+    genre: "fiction",
+    publish: 2022,
+    edition: 3,
+  },
+  {
+    title: "book4",
+    genre: "non-fiction",
+    publish: 2023,
+    edition: 4,
+  },
+  {
+    title: "book5",
+    genre: "fiction",
+    publish: 2024,
+    edition: 5,
+  },
+];
+
+const usebooks = books.filter((book) => book.genre === "non-fiction");
+console.log(usebooks);
