8d652cafe3fdc26baebdf198b7a7caf614873200
ujval <naveenkuamr.gs@isteer.com>
2024-07-12 14:43:19 +0530

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,16 @@
+let myArr = ["batman", "flash", "spiderman", "hulk"];
+
+let arr = 0;
+while (arr < myArr.length) {
+  //   console.log(`my super hero is ${myArr[arr]}`);
+  arr = arr + 1;
+}
+
+//do while loop
+// let score = 1;
+let score = 11;
+
+do {
+  console.log(`your score is ${score}`);
+  score = score + 1;
+} while (score <= 10);
