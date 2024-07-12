0fa8b62eedcb2525493f5fbf132aaffa6e031d33
ujval <naveenkuamr.gs@isteer.com>
2024-07-12 16:46:42 +0530

    Array of objects

---
+++ 05_Iterations/five.js
@@ -0,0 +1,29 @@
+const coding = ["js", "php", "ruby", "java", "html", "css"];
+
+// coding.forEach(function (item) {
+//   console.log(item);
+// });
+
+coding.forEach((item) => {
+  //console.log(item);
+});
+
+//Array of objects
+const myCoding = [
+  {
+    language: "javascript",
+    languageFileName: "Js",
+  },
+  {
+    language: "java",
+    languageFileName: "java",
+  },
+  {
+    language: "C sharp",
+    languageFileName: "C#",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item);
+});
